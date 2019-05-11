da0ba0f7afa02107a43b54ca7dc0b250500e3ed3
Slooowpoke <malii.coxon@gmail.com>
2019-05-11 21:29:10 +0100

    Refactored test for lunar initial state

--- src/tests/reducers/lunarCalculationsReducer.test.js
+++ src/tests/reducers/lunarCalculationsReducer.test.js
@@ -1,12 +1,15 @@
 import lunarCalculations from "../../reducers/lunarCalculations";
+import { GET_NEXT_PHASE_SUCCESS } from "../../actions/nextPhaseCalculations";
+
+const initialStateMock = {
+  nextLunarPhase: null,
+  currentLunarPhase: null,
+  phaseForCurrentLocation: null,
+  ageInLunarYears: null,
+};
 
 describe("lunar calculations reducer", () => {
   it("should return the initial state", () => {
-    expect(lunarCalculations(undefined, {})).toEqual({
-      nextLunarPhase: null,
-      currentLunarPhase: null,
-      phaseForCurrentLocation: null,
-      ageInLunarYears: null,
-    });
+    expect(lunarCalculations(undefined, {})).toEqual(initialStateMock);
   });
 });
