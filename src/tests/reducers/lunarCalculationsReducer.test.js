4eec67cf64eac4152de9f73595fa53d1be3b412b
Slooowpoke <malii.coxon@gmail.com>
2019-05-11 21:29:35 +0100

    Added test for GET_NEXT_PHASE_SUCCESS action

--- src/tests/reducers/lunarCalculationsReducer.test.js
+++ src/tests/reducers/lunarCalculationsReducer.test.js
@@ -12,4 +12,31 @@ describe("lunar calculations reducer", () => {
   it("should return the initial state", () => {
     expect(lunarCalculations(undefined, {})).toEqual(initialStateMock);
   });
+
+  it("should handle GET_NEXT_PHASE_SUCCESS", () => {
+    const action = {
+      type: GET_NEXT_PHASE_SUCCESS,
+      body: {
+        error: false,
+        apiversion: "2.2.0",
+        year: 2019,
+        month: 5,
+        day: 13,
+        numphases: 1,
+        datechanged: false,
+        phasedata: [
+          {
+            phase: "Full Moon",
+            date: "2019 May 18",
+            time: "21:11"
+          }
+        ]
+      }
+    };
+
+    expect(lunarCalculations(initialStateMock, action)).toEqual({
+      ...initialStateMock,
+      nextLunarPhase: action.body.phasedata[0]
+    });
+  });
 });
