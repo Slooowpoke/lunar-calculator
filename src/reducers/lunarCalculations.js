91f21a0091773511df85d1a3da7dcfcf8f3b968e
Slooowpoke <malii.coxon@gmail.com>
2019-05-12 15:12:17 +0100

    Added GET_PHASE_FOR_BIRTHDAY case to reducer

--- src/reducers/lunarCalculations.js
+++ src/reducers/lunarCalculations.js
@@ -1,5 +1,6 @@
 import { GET_NEXT_PHASE_SUCCESS } from "../actions/nextPhaseCalculations";
 import { GET_CURRENT_LUNAR_PHASE_SUCCESS } from "../actions/currentPhaseCalculations";
+import { GET_PHASE_FOR_BIRTHDAY_SUCCESS } from "../actions/phaseForBirthdayCalculations";
 
 const initialState = {
   nextLunarPhase: null,
@@ -40,7 +41,25 @@ const lunarCalculations = (state = initialState, action) => {
           percentage: "100%"
         }
       };
-
+    case GET_PHASE_FOR_BIRTHDAY_SUCCESS:
+      if (action.body.curphase) {
+        // If we are in a transition phase, apply the curphase
+        return {
+          ...state,
+          phaseForBirthday: {
+            phase: action.body.curphase,
+            percentage: action.body.fracillum
+          }
+        };
+      }
+      // Otherwise apply the closestphase from the API
+      return {
+        ...state,
+        phaseForBirthday: {
+          phase: action.body.closestphase.phase,
+          percentage: "100%"
+        }
+      };
     default:
       return state;
   }
