697144b9ffd085965a5cd4a854cb4475cc0ff10d
Slooowpoke <mail@malicoxon.co.uk>
2019-05-12 15:35:56 +0100

    Added PhaseForBirthdayContainer component

---
+++ src/components/PhaseForBirthdayContainer.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import FormatDateToAmericanDateString from "../utils/FormatDateToAmericanDateString";
+import { getPhaseForBirthday } from "../actions/phaseForBirthdayCalculations";
+import PhaseForBirthday from "./PhaseForBirthday";
+
+class PhaseForBirthdayContainer extends React.Component {
+  componentDidMount() {
+    this.fetchPhaseForDateOnLocation = this.fetchPhaseForDateOnLocation.bind(
+      this
+    );
+  }
+
+  fetchPhaseForDateOnLocation = (inputDate) => {
+    const { props } = this;
+
+    const date = new Date(inputDate);
+    props.loadPhaseForBirthday(FormatDateToAmericanDateString(date));
+  }
+
+
+  render() {
+    const { phaseForBirthday, resetPhaseForBirthday } = this.props;
+
+    return (
+      <PhaseForBirthday
+        {...phaseForBirthday}
+        fetchPhaseForDateOnLocation={this.fetchPhaseForDateOnLocation}
+        resetPhaseForBirthday={resetPhaseForBirthday}
+      />
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadPhaseForBirthday: date => {
+      dispatch(getPhaseForBirthday(date));
+    },
+    resetPhaseForBirthday: () => {
+      dispatch(resetPhaseForBirthday());
+    }
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+    phaseForBirthday: state.lunarCalculations.phaseForBirthday
+  };
+};
+
+PhaseForBirthdayContainer.propTypes = {
+  phaseForBirthday: PropTypes.shape({
+    phase: PropTypes.string,
+    percentage: PropTypes.string
+  })
+};
+
+PhaseForBirthdayContainer.defaultProps = {
+  phaseForBirthday: {
+    phase: "",
+    percentage: ""
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PhaseForBirthdayContainer);
