9c6833e2a3da92d524e36932da228fd3e3848987
Slooowpoke <malii.coxon@gmail.com>
2019-05-12 16:44:23 +0100

    Added quick fix for FlexDirection on body

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,8 @@ const styles = {
       display: "flex",
       alignItems: "center",
       justifyContent: "center",
-      height: "100vh"
+      height: "100vh",
+      flexDirection: "column"
     },
     h1: {
       fontFamily: "Baloo Bhai",
