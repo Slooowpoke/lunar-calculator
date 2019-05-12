4db2424e8aa3855091c32d86db6fdd5257029367
Slooowpoke <mail@malicoxon.co.uk>
2019-05-12 13:47:39 +0100

    Added GET_CURRENT_LUNAR_PHASE_SUCCESS to lunarCalculations reducer

--- src/reducers/lunarCalculations.js
+++ src/reducers/lunarCalculations.js
@@ -1,4 +1,5 @@
 import { GET_NEXT_PHASE_SUCCESS } from "../actions/nextPhaseCalculations";
+import { GET_CURRENT_LUNAR_PHASE_SUCCESS } from "../actions/currentPhaseCalculations";
 
 const initialState = {
   nextLunarPhase: null,
@@ -20,6 +21,28 @@ const lunarCalculations = (state = initialState, action) => {
         ...state,
         nextLunarPhase: action.body.phasedata[0]
       };
+    case GET_CURRENT_LUNAR_PHASE_SUCCESS:
+      if(action.body.curphase){
+        // If we are in a transition phase, apply the curphase
+        return {
+          ...state,
+          phaseForCurrentLocation: {
+            phase: action.body.curphase,
+            percentage: action.body.fracillum
+          }
+        };
+
+      }else{
+        // Otherwise apply the closestphase from the API
+        return {
+          ...state,
+          phaseForCurrentLocation: {
+            phase: action.body.closestphase.phase,
+            percentage: "100%"
+          }
+        };
+      }
+
     default:
       return state;
   }
